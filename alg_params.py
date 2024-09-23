82d7e58e867e5762ae872b3e692c707675bbf79f
SwastiKh <swastikhurana0800@gmail.com>
2024-09-23 20:43:02 +0200

    algo changes and params file

---
+++ alg_params.py
@@ -0,0 +1,15 @@
+
+# dummy parameters
+dom_u = 1
+dom_l = -1
+npop = 100
+gens = 30
+mutation = 0.2
+last_best = 0
+
+#  Other parameters
+seed = 1234
+experiment_name = 'crossover_baseline_enemy1_'+str(seed)
+
+
+alg_args = {'experiment_name': experiment_name, 'seed': seed, 'dom_u': dom_u, 'dom_l': dom_l, 'npop': npop, 'gens': gens, 'mutation': mutation, 'last_best': last_best}
--- optimization_dummy.py
+++ optimization_dummy.py
@@ -10,10 +10,57 @@ import sys
 
 from evoman.environment import Environment
 from demo_controller import player_controller
+from alg_params import *
 
 # imports other libs
 import numpy as np
 import os
+import time
+
+
+#DIVERSITY OF THE POPULATION AS IMPORTANT AS THE FITNESS OF THE INDIVIDUALS
+
+# choose this for not using visuals and thus making experiments faster
+headless = True
+if headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+
+experiment_name = 'crossover_baseline_enemy1'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+
+n_hidden_neurons = 128
+
+# initializes simulation in individual evolution mode, for single static enemy.
+env = Environment(experiment_name=experiment_name,
+                  enemies=[1], # enemy number 1 is selected
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden_neurons),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest",
+                  visuals=False)
+
+# default environment fitness is assumed for experiment
+
+env.state_to_log() # checks environment state
+
+
+####   Optimization for controller solution (best genotype-weights for phenotype-network): Ganetic Algorihm    ###
+
+ini = time.time()  # sets time marker
+
+
+# genetic algorithm params
+
+run_mode = 'train' # train or test
+
+# number of weights for multilayer with 128 hidden neurons
+n_weights = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
+
+
+
 
 # runs simulation
 def simulation(env,x):
@@ -32,7 +79,7 @@ def main():
         os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 
-    experiment_name = 'optimization_test'
+    # experiment_name = 'optimization_test'
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
@@ -40,7 +87,7 @@ def main():
 
     # initializes simulation in individual evolution mode, for single static enemy.
     env = Environment(experiment_name=experiment_name,
-                    enemies=[2],
+                    enemies=[1],
                     playermode="ai",
                     player_controller=player_controller(n_hidden_neurons), # you  can insert your own controller here
                     enemymode="static",
@@ -50,7 +97,7 @@ def main():
 
 
     # number of weights for multilayer with 10 hidden neurons
-    n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
+    n_weights = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
 
     # start writing your own code from here
 
